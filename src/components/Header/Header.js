275fc213208f6d3da47d19bc07b883e0b72d3cfd
Admin <admin@MacBook-Air-cua-Admin.local>
2024-07-23 01:41:52 +0700

    Design header homepage

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                {/* <Navbar.Brand href="">Quiz</Navbar.Brand> */}
+                <NavLink to='/' className='navbar-brand'>Quiz</NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink to='/' className='nav-link'>Home</NavLink>
+                        <NavLink to='/users' className='nav-link'>Users</NavLink>
+                        <NavLink to='/admins' className='nav-link'>Admin</NavLink>
+                    </Nav>
+                    <Nav>
+                        <button className='btn-login'>Login</button>
+                        <button className='btn-signup'>Signup</button>
+                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Log in</NavDropdown.Item>
+                            <NavDropdown.Item>Log out</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                        </NavDropdown> */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
